bc3c3130ed973e723c5e9cd038e123c4e0f2f2b9
tzhangm <tzhangm@trip.com>
2023-01-09 08:42:24 +0800

    udp

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { prepareInit } from './utils/prepareInit';
 import * as cookieParser from 'cookie-parser';
+import * as compression from 'compression';
+// somewhere in your initialization file
+
 
 async function bootstrap() {
   prepareInit();
   const { AppModule } = await import('./app.module');
 
   const app = await NestFactory.create(AppModule);
+  app.use(compression());
   // somewhere in your initialization file
   app.use(cookieParser());
   app.use(function (req, res, next) {
